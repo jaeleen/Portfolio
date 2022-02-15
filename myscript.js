d9c862e495ddce6ef6107f531bac7f2d59a50833
jaeleen <jaeleen.pg@gmail.com>
2022-02-14 22:12:20 -0600

    Update index.html

--- myscript.js
+++ myscript.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 
     // typing animation script 
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Designer"],
+        strings: ["Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
